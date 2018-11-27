3490e03783a868ab26f271d67a06a157eb535945
出内将夫 <ideuchi@mark-mac.local>
2018-11-27 10:45:20 +0900

    change folder name

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 import sys
 
 sys.path.append('./src')
-sys.path.append('./tests')
+sys.path.append('./test')
 
 setup(
 	name = 'Hoge',
 	version = '0.1',
-	description = 'This is test codes for travis ci',
+	description = 'This is test codes for Travis CI',
 	packages = find_packages(),
-	test_suite = 'test.suite'
+	test_suite = 'hogetest.suite'
 )
 
