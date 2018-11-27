d02fffed5bc64fdacf2f496b4b57e478414ae04a
出内将夫 <ideuchi@mark-mac.local>
2018-11-27 10:03:51 +0900

    Add comma to setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ sys.path.append('./tests')
 
 setup(
 	name = 'Hoge',
-	version = '0.1'
+	version = '0.1',
 	description = 'This is test codes for travis ci',
 	packages = find_packages(),
 	test_suite = 'hoge_test.suite'
