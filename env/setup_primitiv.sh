d6a2eba031cc18ecb8ca0f0b7982e45d89ccb1df
出内将夫 <ideuchi@mark-mac.local>
2018-11-29 10:23:05 +0900

    add environment settings

--- env/setup_primitiv.sh
+++ env/setup_primitiv.sh
@@ -9,14 +9,14 @@ mkdir build
 cd build
 cmake ..
 make
-make install
+sudo make install
 
 # build Python3 primitiv
 pip3 install numpy cython cmake scikit-build
 unzip -qo primitiv-python-develop.zip
 cd primitiv-python-develop
 python3 ./setup.py build
-python3 ./setup.py install
+sudo python3 ./setup.py install
 
 return 0
 
