511904722a7732e724b164e4a23db44d18f66076
出内将夫 <ideuchi@mark-mac.local>
2018-11-29 11:37:09 +0900

    add environment settings

--- env/setup_primitiv.sh
+++ env/setup_primitiv.sh
@@ -19,6 +19,7 @@ sudo ln -s /usr/local/include/primitiv/core/shape.h /usr/local/include/primitiv/
 sudo ln -s /usr/local/include/primitiv/core/device.h /usr/local/include/primitiv/device.h
 sudo ln -s /usr/local/include/primitiv/core/tensor.h /usr/local/include/primitiv/tensor.h
 sudo ln -s /usr/local/include/primitiv/core/graph.h /usr/local/include/primitiv/graph.h
+sudo ln -s /usr/local/include/primitiv/core/initializer.h /usr/local/include/primitiv/initializer.h
 
 # build Python3 primitiv
 pip3 install numpy cython cmake scikit-build
