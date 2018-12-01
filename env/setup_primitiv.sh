17ad320196c2cde1c1e91ade2a46d7c8fa87fce9
出内将夫 <ideuchi@mark-mac.local>
2018-12-01 15:55:13 +0900

    add environment settings

--- env/setup_primitiv.sh
+++ env/setup_primitiv.sh
@@ -34,6 +34,7 @@ unzip -qo primitiv-python-develop.zip
 cd primitiv-python-develop
 sed -i -e "s/\*args, \*\*kwargs,/\*args,/g" setup.py
 sed -i -e 's/extra_compile_args=\["-std=c++11"\],/extra_compile_args=\["-std=c++11"\], \*\*kwargs/g' setup.py
+sed -i -e 's/setup_kwargs,/setup_kwargs/g' setup.py
 python3 ./setup.py build
 sudo python3 ./setup.py install
 
