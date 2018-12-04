8e7944a1f9fc5578cf9a9f6a307ddc4762896804
出内将夫 <ideuchi@mark-mac.local>
2018-12-05 07:35:36 +0900

    delete init method

--- src/hoge.py
+++ src/hoge.py
@@ -8,30 +8,30 @@ from primitiv import initializers as I
 from primitiv import optimizers as O
 
 class Hoge:
+	# primitiv initialization
+	dev = D.Naive()
+	Device.set_default(dev)
+	g = Graph()
+	Graph.set_default(g)
+
 	def index(self):
 		return True
 
-	def primitiv_init(self):
-		dev = D.Naive()
-		Device.set_default(dev)
-		g = Graph()
-		Graph.set_default(g)
-
 	def primitiv_test1(self):
-		primitiv_init(self)
+		primitiv_init()
 		x = F.input(np.array([[1], [2], [3]]))
 		y = 2 * x + 3
 		return y.to_list()
 
 	def primitiv_test2(self):
-		primitiv_init(self)
+		primitiv_init()
 		x = F.input(np.array([[1], [2]]))
 		a = F.input(np.array([[1, 2], [1, 2]]))
 		y = F.matmul(a, x)
 		return y.to_list()
 
 	def primitiv_xor_test(self):
-		primitiv_init(self)
+		primitiv_init()
 		input_data = [
 			np.array([[ 1], [ 1]]),
 			np.array([[-1], [ 1]]),
