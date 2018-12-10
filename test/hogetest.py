e83a9663a65a47f49ddab37d9d1b2c458df5d8fb
出内将夫 <ideuchi@mark-mac.local>
2018-12-10 23:44:56 +0900

    add assert

--- test/hogetest.py
+++ test/hogetest.py
@@ -5,18 +5,19 @@ from hoge import Hoge
 
 class HogeTest(unittest.TestCase):
 
-	def SetUp(self):
-		print('setup')
+    def SetUp(self):
+        print('setup')
 
-	def test_first(self):
-		print('test_first')
+    def test_first(self):
+        print('test_first')
 
-	def test_hoge(self):
-		hoge = Hoge()
-		self.assertTrue(hoge.index())
-		self.assertEqual(hoge.primitiv_test1(), [5.0, 7.0, 9.0])
-		self.assertEqual(hoge.primitiv_test2(), [5.0, 5.0])
-		self.assertTrue(len(hoge.primitiv_xor_test()) == 4)
+    def test_hoge(self):
+        hoge = Hoge()
+        self.assertTrue(hoge.index())
+        self.assertEqual(hoge.primitiv_test1(), [5.0, 7.0, 9.0])
+        self.assertEqual(hoge.primitiv_test2(), [5.0, 5.0])
+        self.assertTrue(len(hoge.primitiv_xor_test()) == 4)
+        self.assertLess(hoge.primitiv_xor_test(), 1.5)
 
 def suite():
 	suite = unittest.TestSuite()
